ae488234d1b56f9df4c2146c962104fe5841b538
Aarish A. Sheikh <aarishsheikh3757@gmail.com>
2025-07-01 20:04:19 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/com/aarish/PwdGenerator/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.aarish.PwdGenerator.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // apply to all paths
+                        .allowedOrigins("https://password-generator-backend-production-c5c7.up.railway.app") // frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
+
